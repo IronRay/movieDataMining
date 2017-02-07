47928cb8ba1ec0ccc231987c88d1241c2a75e9d7
Ray <hu.ray@foxmail.com>
2017-02-07 23:47:44 +0800

    Update.

--- miningBot/miner/config/dbConfig.py
+++ miningBot/miner/config/dbConfig.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 dbConfig = {
-    'host': 'localhost',
-    'user': 'ray',
-    'password': 'codeForRay123',
-    'db': 'ZHIHU_SCRAPING'
+    'host': '',
+    'user': '',
+    'password': '',
+    'db': ''
 }
